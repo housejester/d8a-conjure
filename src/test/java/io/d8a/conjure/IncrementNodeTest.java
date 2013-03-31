daf3bfa066b5bddf3bb704292b4a492cd4dafda3
Eugen Dinca <decostin@gmail.com>
2013-03-30 23:52:26 -0400

    Added simple increment node.
    Increment is a simple node that generates an ever increasing value,
    starting with value and changing by step (defaults to 1).

---
+++ src/test/java/io/d8a/conjure/IncrementNodeTest.java
@@ -0,0 +1,58 @@
+package io.d8a.conjure;
+
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import static junit.framework.Assert.assertEquals;
+
+@Test
+public class IncrementNodeTest {
+    private ConjureTemplate samples;
+
+    @BeforeMethod
+    public void setUp() throws Exception {
+        samples = new ConjureTemplate();
+        samples.addNodeType("increment", IncrementNode.class);
+    }
+
+    public void generatesIncrementalNumbers(){
+        samples.addFragment("sample", "${type:\"increment\", value:1, step:3}");
+        long firstValue = Long.valueOf(samples.conjure());
+        assertEquals(firstValue, 1);
+        long secondValue = Long.valueOf(samples.conjure());
+        assertEquals(secondValue, 4);
+    }
+
+    public void generatesIncrementalNumbersDefaultStep(){
+        samples.addFragment("sample", "${type:\"increment\", value:42}");
+        long firstValue = Long.valueOf(samples.conjure());
+        assertEquals(firstValue, 42);
+        long secondValue = Long.valueOf(samples.conjure());
+        assertEquals(secondValue, 43);
+    }
+
+    public void generatesIncrementalNegativeValue(){
+        samples.addFragment("sample", "${type:\"increment\", value:-1}");
+        long firstValue = Long.valueOf(samples.conjure());
+        assertEquals(firstValue, -1);
+        long secondValue = Long.valueOf(samples.conjure());
+        assertEquals(secondValue, 0);
+    }
+
+    public void generatesIncrementalNegativeStep(){
+        samples.addFragment("sample", "${type:\"increment\", value:12, step:-2}");
+        long firstValue = Long.valueOf(samples.conjure());
+        assertEquals(firstValue, 12);
+        long secondValue = Long.valueOf(samples.conjure());
+        assertEquals(secondValue, 10);
+    }
+
+    public void generatesIncrementalNegativeValueAndStep(){
+        samples.addFragment("sample", "${type:\"increment\", value:-1, step:-3}");
+        long firstValue = Long.valueOf(samples.conjure());
+        assertEquals(firstValue, -1);
+        long secondValue = Long.valueOf(samples.conjure());
+        assertEquals(secondValue, -4);
+    }
+
+}
