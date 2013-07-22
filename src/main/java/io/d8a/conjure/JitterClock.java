2c8a516146cbbbbc6ecfea837c98ba93e5f2b08f
Dhruv Parthasarathy <dhroovie@gmail.com>
2013-07-22 12:36:21 -0700

    can conjure data with custom cardinality, types, and numcolumns

--- src/main/java/io/d8a/conjure/JitterClock.java
+++ src/main/java/io/d8a/conjure/JitterClock.java
@@ -1,30 +1,36 @@
 package io.d8a.conjure;
 
-public class JitterClock implements Clock {
-    private Clock targetClock;
-    private Jitter jitter;
+public class JitterClock implements Clock
+{
+  private Clock targetClock;
+  private Jitter jitter;
 
-    public JitterClock(Clock targetClock, int jitterLow, int jitterHigh) {
-        this(targetClock, new MinMax(jitterLow, jitterHigh));
-    }
+  public JitterClock(Clock targetClock, int jitterLow, int jitterHigh)
+  {
+    this(targetClock, new MinMax(jitterLow, jitterHigh));
+  }
 
-    public JitterClock(Clock targetClock, Jitter jitter) {
-        this.targetClock = targetClock;
-        this.jitter = jitter;
-    }
+  public JitterClock(Clock targetClock, Jitter jitter)
+  {
+    this.targetClock = targetClock;
+    this.jitter = jitter;
+  }
 
-    @Override
-    public long currentTimeMillis() {
-        return targetClock.currentTimeMillis() + jitter.nextValue();
-    }
+  @Override
+  public long currentTimeMillis()
+  {
+    return targetClock.currentTimeMillis() + jitter.nextValue();
+  }
 
-    @Override
-    public void sleep(long millis) {
-        targetClock.sleep(millis);
-    }
+  @Override
+  public void sleep(long millis)
+  {
+    targetClock.sleep(millis);
+  }
 
-    @Override
-    public String toString() {
-        return "Jitter";
-    }
+  @Override
+  public String toString()
+  {
+    return "Jitter";
+  }
 }
--- src/main/java/io/d8a/conjure/MultiPrinter.java
+++ src/main/java/io/d8a/conjure/MultiPrinter.java
@@ -3,26 +3,33 @@ package io.d8a.conjure;
 import java.util.Arrays;
 import java.util.Collection;
 
-public class MultiPrinter implements Printer{
-    private Collection<Printer> printers;
-    public MultiPrinter(Collection<Printer> printers){
-        this.printers = printers;
-    }
-    public MultiPrinter(Printer...printers){
-        this(Arrays.asList(printers));
-    }
+public class MultiPrinter implements Printer
+{
+  private Collection<Printer> printers;
 
-    @Override
-    public void print(String message) {
-        for(Printer printer : printers){
-            printer.print(message);
-        }
-    }
+  public MultiPrinter(Collection<Printer> printers)
+  {
+    this.printers = printers;
+  }
 
-    @Override
-    public String toString() {
-        return "MultiPrinter{" +
-                "printers=" + printers +
-                '}';
+  public MultiPrinter(Printer... printers)
+  {
+    this(Arrays.asList(printers));
+  }
+
+  @Override
+  public void print(Object message)
+  {
+    for (Printer printer : printers) {
+      printer.print(message);
     }
+  }
+
+  @Override
+  public String toString()
+  {
+    return "MultiPrinter{" +
+           "printers=" + printers +
+           '}';
+  }
 }
