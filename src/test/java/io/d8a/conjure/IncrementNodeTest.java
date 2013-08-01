4c97f374ec32d91f44860c565785117fad551b1f
James Estes <james.estes@gmail.com>
2013-08-01 08:05:04 -0600

    Simplified tests for increment node.

--- src/test/java/io/d8a/conjure/IncrementNodeTest.java
+++ src/test/java/io/d8a/conjure/IncrementNodeTest.java
@@ -17,52 +17,40 @@ public class IncrementNodeTest {
 
     public void generatesIncrementalNumbers(){
         samples.addFragment("sample", "${type:\"increment\", value:1, step:3}");
-        long firstValue = Long.valueOf(samples.conjure());
-        assertEquals(firstValue, 1);
-        long secondValue = Long.valueOf(samples.conjure());
-        assertEquals(secondValue, 4);
+        assertEquals(conjureNext(), 1);
+        assertEquals(conjureNext(), 4);
     }
 
     public void generatesIncrementalNumbersDefaultStep(){
         samples.addFragment("sample", "${type:\"increment\", value:42}");
-        long firstValue = Long.valueOf(samples.conjure());
-        assertEquals(firstValue, 42);
-        long secondValue = Long.valueOf(samples.conjure());
-        assertEquals(secondValue, 43);
+        assertEquals(conjureNext(), 42);
+        assertEquals(conjureNext(), 43);
     }
 
     public void generatesIncrementalNegativeValue(){
         samples.addFragment("sample", "${type:\"increment\", value:-1}");
-        long firstValue = Long.valueOf(samples.conjure());
-        assertEquals(firstValue, -1);
-        long secondValue = Long.valueOf(samples.conjure());
-        assertEquals(secondValue, 0);
+        assertEquals(conjureNext(), -1);
+        assertEquals(conjureNext(), 0);
     }
 
     public void generatesIncrementalNegativeStep(){
         samples.addFragment("sample", "${type:\"increment\", value:12, step:-2}");
-        long firstValue = Long.valueOf(samples.conjure());
-        assertEquals(firstValue, 12);
-        long secondValue = Long.valueOf(samples.conjure());
-        assertEquals(secondValue, 10);
+        assertEquals(conjureNext(), 12);
+        assertEquals(conjureNext(), 10);
     }
 
     public void generatesIncrementalNegativeValueAndStep(){
         samples.addFragment("sample", "${type:\"increment\", value:-1, step:-3}");
-        long firstValue = Long.valueOf(samples.conjure());
-        assertEquals(firstValue, -1);
-        long secondValue = Long.valueOf(samples.conjure());
-        assertEquals(secondValue, -4);
+        assertEquals(conjureNext(), -1);
+        assertEquals(conjureNext(), -4);
     }
 
     public void rollsNumbersAfterSpecifiedNumberOfSteps(){
         samples.addFragment("sample", "${type:\"increment\", value:42, rollAfterSteps:2}");
-        long firstValue = Long.valueOf(samples.conjure());
-        assertEquals(firstValue, 42);
-        long secondValue = Long.valueOf(samples.conjure());
-        assertEquals(secondValue, 43);
-        long thirdValue = Long.valueOf(samples.conjure());
-        assertEquals(thirdValue, 42);
+        assertEquals(conjureNext(), 42);
+        assertEquals(conjureNext(), 43);
+
+        assertEquals(conjureNext(), 42);
     }
 
     public void generatesIncrementingValuesAfterRoll(){
